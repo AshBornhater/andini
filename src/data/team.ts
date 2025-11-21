235c33f1faeaf7ec4e7d004056dfcbade1fa6493
chandrak <chandrakusumaa133@gmail.com>
2025-11-21 19:45:40 +0800

    initial commit

---
+++ src/data/team.ts
@@ -0,0 +1,8 @@
+export default [
+  { name: "Andini", role: "Founder", img: "/team/1.jpg" },
+  { name: "Budi", role: "Educator", img: "/team/2.jpg" },
+  { name: "Citra", role: "Content Creator", img: "/team/3.jpg" },
+  { name: "Dewi", role: "Designer", img: "/team/4.jpg" },
+  { name: "Eka", role: "Researcher", img: "/team/5.jpg" },
+  { name: "Fajar", role: "Coordinator", img: "/team/6.jpg" },
+];
